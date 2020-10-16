c9e466bfd51639178a8c7a30d61e06731c083b82
rahmadila1038 <rahmadilan11@gmail.com>
2020-10-16 16:56:48 +0700

    MylistviewApp

---
+++ MyListViewApp/app/src/main/java/com/example/mylistviewapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.mylistviewapp;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private com.example.mylistviewapp.HeroAdapter adapter;
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private ArrayList<com.example.mylistviewapp.Hero> heroes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lv_list);
+        adapter = new com.example.mylistviewapp.HeroAdapter(this);
+        listView.setAdapter(adapter);
+
+        prepare();
+        addItem();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, heroes.get(i).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            com.example.mylistviewapp.Hero hero = new com.example.mylistviewapp.Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+
+        adapter.setHeroes(heroes);
+    }
+}
